ee9b14abf4adc146e157a5f0ac739557f701403e
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-05-03 20:27:21 +0900

    First Commit

---
+++ src/main/java/com/hexagonal/hexagonal_architecture_template/HexagonalArchitectureTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonal_architecture_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hexagonal/hexagonal_architecture_template/HexagonalArchitectureTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonal_architecture_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
